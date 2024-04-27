c132e436e583dbc8da430a8baf1563da33b4aa3a
mafei <mafei.dev@gmail.com>
2024-04-28 00:00:19 +0530

    init2

---
+++ docker-push.sh
@@ -0,0 +1,3 @@
+./mvnw clean install -DskipTests=true
+docker build -t mafeidev/k8s-leader-election-spring-demo:1.0.0 .
+docker push mafeidev/k8s-leader-election-spring-demo:1.0.0
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-./mvnw clean install -DskipTests=true
-docker build -t mafeidev/k8s-leader-election-demo-spring-2:1.0.1 .
-docker push mafeidev/k8s-leader-election-demo-spring-2:1.0.1
+source ./docker-push.sh
+
+kubectl apply -f RBAC.yaml
+kubectl apply -f deployment.yaml
+
